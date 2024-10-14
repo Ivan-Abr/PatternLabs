207ccfbb7c2ea45876b727ed2e82c777a6c92581
Ivan Abramov <ivabramov16@gmail.com>
2024-10-14 19:13:39 +0300

    ЛР2 - fix: Удаление ненужного конструктора

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -80,22 +80,6 @@ class Student(
         private val telegramRegex = Regex("^@[A-Za-z0-9_]{5,32}\$")
         private val gitRegex = Regex("(https?:\\/\\/)?(www\\.)?(github\\.com|gitlab\\.com|bitbucket\\.org)\\/[A-Za-z0-9_.-]+\\/[A-Za-z0-9_.-]+(\\.git)?\$")
 
-
-        fun fromStringToStudent(input: String): Student {
-            val parsedData = input.split("; ")
-            val id = 1337
-            return Student(
-                id,
-                parsedData[0],
-                parsedData[1],
-                parsedData[2],
-                parsedData.getOrNull(3),
-                parsedData.getOrNull(4),
-                parsedData.getOrNull(5),
-                parsedData.getOrNull(6)
-            )
-        }
-
         fun isTelephoneNumberValid(phone: String): Boolean {
             return telephoneRegex.matches(phone)
         }
