660d8a25696050006067784ddec8dcc9faefcd65
Ivan Abramov <ivabramov16@gmail.com>
2024-10-14 15:26:28 +0300

    ЛР2 - fix: Исправление порядка в массиве для конструктора ShortStudent

--- src/main/kotlin/ShortStudent.kt
+++ src/main/kotlin/ShortStudent.kt
@@ -19,12 +19,12 @@ class ShortStudent(
 
     constructor(id: Int, data: String): this(
         id = id,
-        firstName = data.split(";")[1],
-        lastName = data.split(";")[2],
-        git = data.split(";").getOrNull(3),
-        telephone = data.split(";").getOrNull(4),
-        telegram = data.split(";").getOrNull(5),
-        mail = data.split(";").getOrNull(6)
+        firstName = data.split(";")[0],
+        lastName = data.split(";")[1],
+        git = data.split(";").getOrNull(2),
+        telephone = data.split(";").getOrNull(3),
+        telegram = data.split(";").getOrNull(4),
+        mail = data.split(";").getOrNull(5)
     )
     override fun toString(): String {
         return "ShortStudent($id; $firstName; $lastName; $git; $telephone; $telegram; $mail)"
