ad874e4aa7512723c3fa84c1c4a5d69ecffcdf80
Ivan Abramov <ivabramov16@gmail.com>
2024-10-14 21:57:00 +0300

    ЛР2 - Задание 2.4: Реализация необходимых методов класса DataList

--- src/main/kotlin/DataList.kt
+++ src/main/kotlin/DataList.kt
@@ -1,4 +1,21 @@
 open class DataList<T: Comparable<T>>(private val elements: List<T>) {
     private val sortedElements: List<T> = elements.sorted()
     private val selectedElements = mutableSetOf<Int>()
+
+    fun select(number: Int){
+        if (number in sortedElements.indices)
+            selectedElements.add(number)
+        else
+            throw IndexOutOfBoundsException("number $number is not valid")
+    }
+
+    fun getSelected(): List<Int> = selectedElements.toList()
+
+    open fun getNames():List<String>{
+        throw UnsupportedOperationException("should be realized in nested class")
+    }
+
+    open fun getData(): DataTable<String>{
+        throw UnsupportedOperationException("should be realized in nested class")
+    }
 }
