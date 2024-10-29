a725de0084ac0ac0fc36e75f05b02f26639c6dc2
Ivan Abramov <ivabramov16@gmail.com>
2024-10-29 17:59:44 +0300

    ЛР3 - Подключение библиотек для работы с json и yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "ru.kubsu"
@@ -10,6 +11,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation(kotlin("stdlib"))
+    implementation("com.charleskorn.kaml:kaml:0.52.0")
+    implementation("org.postgresql:postgresql:42.7.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
